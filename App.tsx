d20a71bc48f3f4f4040bdfc317937ecba3fa07f8
linhareshian <hian.linhares@rethink.dev>
2025-01-26 15:27:00 -0300

    aprendendo a criar folhas de estilo e utilizar imagens

--- App.tsx
+++ App.tsx
@@ -1,14 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image} from 'react-native';
+import symbolon from './assets/pictures/symbol-on.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Hian</Text>
-      <StatusBar style="auto" />
+      <Text style = {{color: 'white', backgroundColor: 'black', fontSize: 20 }}>Hello Hian</Text>
+      <Text style = {{color: 'white', backgroundColor: 'black', fontSize: 20 }}>Dev mobile react native</Text>
+      <Text style = {styles2.titleStyle}>teste de folha de estilo</Text> 
+      <Text style = {styles2.teste}>teste de folha de estilo</Text>
+      <Image source={symbolon}></Image>
     </View>
+        
   );
 }
+
+// criando uma folha de estilo (baseado em css)
+const styles2 = StyleSheet.create({
+  titleStyle:{
+    fontSize:15,
+    color: 'red'
+  },
+  teste:{
+    fontSize:30,
+    color: 'green'
+  }
+})
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +34,20 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   }
 });
+
+
+/*
+ FOLHA DE ESTILO BASEADA EM CSS
+const styles2 = StyleSheet.create({
+  titleStyle:{
+    fontSize:15,
+    color: 'red'
+  }
+})
+
+LINHA DE ESTILO 
+  <Text style = {{color: 'white', backgroundColor: 'black', fontSize: 20 }}>Dev mobile react native</Text>
+
+*/
+
+
