f6f92198d237da8b02895a7cc08dd5ac7fc39703
linhareshian <hian.linhares@rethink.dev>
2025-01-27 17:47:01 -0300

    adicionando lógica em um componente

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
 import { StyleSheet, Text, View,Image} from 'react-native';
 import symbolon from './assets/pictures/symbol-on.png';
+import symboloff from './assets/pictures/symbol-off.png';
+
 
 export default function App() {
+  const isActive = false;
+
   return (
     <View style={styles.container}>
       <Text style = {{color: 'white', backgroundColor: 'black', fontSize: 20 }}>Hello Hian</Text>
       <Text style = {{color: 'white', backgroundColor: 'black', fontSize: 20 }}>Dev mobile react native</Text>
       <Text style = {styles2.titleStyle}>teste de folha de estilo</Text> 
       <Text style = {styles2.teste}>teste de folha de estilo</Text>
-      <Image source={symbolon}></Image>
+      <Image source={isActive ? symbolon : symboloff}></Image>
     </View>
         
   );
@@ -18,18 +22,19 @@ export default function App() {
 const styles2 = StyleSheet.create({
   titleStyle:{
     fontSize:15,
-    color: 'red'
+    color: 'black'
   },
   teste:{
     fontSize:30,
-    color: 'green'
+    color: 'black'
   }
 })
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   }
--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,5 @@
-declare module '*.png'
-declare module '*.bmp'
+declare module '*.png' {
+    const value: any;
+    export default value;
+  }
+  
