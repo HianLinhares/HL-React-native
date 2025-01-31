5847363cb88f374e25603407eade09fee9a3134a
Hian Linhares <hianfernando@hotmail.com>
2025-01-30 23:33:09 -0300

    utilizando snippets

---
+++ Gerador-de-senhas/src/components/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+import { styles } from './MenuStyles';
+
+export function Menu() {
+  return (
+    <View style={styles.container}>
+        <Text style={styles.container}>React Native</Text>
+    </View>
+  );
+}
+
+//RNBC - CRIAR UM ARQUIVO PADRÃO DE REACT NATIVE
---
+++ Gerador-de-senhas/src/components/Menu/MenuStyles.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+   backgroundColor: 'grey',
+   color: 'white',
+   fontSize: 15,
+   
+  }
+});
+
+
+
+// RNSO - CRIANDO UMA FOLHA DE ESTILO PADRÃO 
