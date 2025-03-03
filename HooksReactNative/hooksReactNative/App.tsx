a2c1639e24d9ec42fa4f236b9408d987f16d09f0
Hian Linhares <hianfernando@hotmail.com>
2025-03-03 02:23:33 -0300

    criando projeto e primeiro useState

---
+++ HooksReactNative/hooksReactNative/App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
+
+
+export default function App() {
+
+  const [number, setnumber] = useState(0)
+
+  return (
+    <View style={styles.container}>
+
+    <TouchableOpacity onPress={()=> setnumber(number+1)}>
+      <Text style = {styles.texto}>{number}</Text>
+    </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto:{
+    fontSize:40
+  }
+});
