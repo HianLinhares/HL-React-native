a8836d65fa1ff5a20f55f0ff895fc27ba89871b8
Hian Linhares <hianfernando@hotmail.com>
2025-03-06 00:14:06 -0300

    useEffect e UseState

--- HooksReactNative/hooksReactNative/App.tsx
+++ HooksReactNative/hooksReactNative/App.tsx
@@ -1,18 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, Button, Alert } from 'react-native';
+import { useState, useEffect } from 'react';
 
 
 export default function App() {
 
-  const [number, setnumber] = useState(0)
+  const [number, setnumber] = useState(0);
+  const [numero, setnumero] = useState(0);
+
+  useEffect(()=>{
+    if(numero===10)
+    Alert.alert("Hian seu lindo")
+  },[numero])
+  
+
+  const incremento = ()=> {
+    if(numero>-1){
+      setnumero((numero=>numero+1))
+    }
+  }
 
   return (
     <View style={styles.container}>
 
     <TouchableOpacity onPress={()=> setnumber(number-1)}>
-      <Text style = {styles.texto}>{number}</Text>
+      <Text style = {styles.texto}>{numero}</Text>
     </TouchableOpacity>
+
+    <Button
+      title='SOMAR'
+      onPress={incremento}
+    ></Button>
+
+    <Button
+      title='SUBTRAIR'
+      onPress={()=> setnumero(numero-1)}
+    ></Button>
+
     </View>
   );
 }
@@ -30,5 +53,8 @@ const styles = StyleSheet.create({
     borderWidth: 10,
     padding:40,
     borderRadius:16,
+  },
+  botoes:{
+    padding:10,
   }
 });
