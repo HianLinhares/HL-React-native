5e241c986abf1f9a026f168ae732eb8d95d0792e
Hian Linhares <hianfernando@hotmail.com>
2025-03-18 23:21:33 -0300

    estruturação do setup do módulo de navegação

---
+++ NavegabilidadeReactNative/NavegationReactNative/App.tsx
@@ -0,0 +1,10 @@
+import { Home } from './Src/screens/Home';
+
+export default function App() {
+  return (
+   <>
+   <Home></Home>
+   </>
+  );
+}
+
---
+++ NavegabilidadeReactNative/NavegationReactNative/Src/screens/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './StyleHome';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+        <Text style = {styles.textos}> Olá Hian Linhares</Text>
+        
+    </View>
+  );
+}
