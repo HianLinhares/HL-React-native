82a731a52cbf31c7a6def73ea81d985023a85cf4
Hian Linhares <hianfernando@hotmail.com>
2025-04-23 23:14:04 -0300

    modulo de navegabilidade

---
+++ NavegationReactNative/navegation-React-Native/routes/stack.routes.tsx
@@ -0,0 +1,15 @@
+import { createStackNavigator } from "@react-navigation/stack";
+
+const {Screen, Navigator} = createStackNavigator();
+import {Home} from '../src/screens/Home'
+import { GalleryScreen } from "../src/screens/Galleryscreen";
+
+export function StackRoutes(){
+    return (
+        <Navigator>
+         
+         
+         
+        </Navigator>
+    )
+}
